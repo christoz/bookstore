825b363b3a6ac41982d2dd186626267a892a4301
Christos Ziogas <cziogas@steficon.gr>
2020-03-31 20:05:56 +0300

    Added header and footer layout components with styling as well

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,1 +1,16 @@
-export { default } from './Header'
+import React from 'react';
+import styles from './header.module.css';
+
+const Header = props => {
+	return (
+		<header className={styles.header}>
+			<div className="container">
+				<div className={styles.headerRow}>
+					{props.children}
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default Header;
