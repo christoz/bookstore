55301b6c4efd13e8088c47d1dec4c3ddb7cd0705
Christos Ziogas <cziogas@steficon.gr>
2020-04-02 11:50:52 +0300

    Created Main layout component

---
+++ src/components/Layout/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Main = props => {
+	return (
+		<main className="main">
+			{props.children}
+		</main>
+	);
+};
+
+Main.propTypes = {
+	children: PropTypes.node.isRequired
+};
+
+export default Main;
