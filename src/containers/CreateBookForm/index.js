5533047ec2e71f5c80fd3d337570b64ebc163699
Christos Ziogas <cziogas@steficon.gr>
2020-04-03 18:54:37 +0300

    Update index.js

--- src/containers/CreateBookForm/index.js
+++ src/containers/CreateBookForm/index.js
@@ -10,7 +10,10 @@ const CreateBookForm = () => {
 		values,
 		handleChange,
 		handleSubmit,
-		errors
+		handleBlur,
+		errors,
+		valid,
+		touched
 	} = useForm({
 		initialValues: {
 			title: '',
@@ -19,8 +22,7 @@ const CreateBookForm = () => {
 			authorName: '',
 			publisher: '',
 			year: '',
-			pageNumbers: '',
-			ISBN: ''
+			pageNumbers: ''
 		},
 		onSubmit: () => console.log('submit'),
 		validationSchema: bookSchema
@@ -28,49 +30,48 @@ const CreateBookForm = () => {
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<pre>{JSON.stringify(errors, null, 2)}</pre>
 			<div className="form-group">
 				<label htmlFor="title">Title</label>
-				<input name="title" type="text" value={values.title} onChange={handleChange} />
-				<FormError>{errors.title}</FormError>
+				<input name="title" type="text" value={values.title} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.title}</FormError>
 			</div>
 			<div className="form-group">
 				<label htmlFor="description">Description</label>
-				<input name="description" type="text" value={values.description} onChange={handleChange} />
-				<FormError>{errors.description}</FormError>
+				<input name="description" type="text" value={values.description} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.description}</FormError>
 			</div>
 			<div className="form-group">
 				<label htmlFor="categories">Categories</label>
-				<input name="categories" type="text" value={values.categories} onChange={handleChange} />
-				<FormError>{errors.categories}</FormError>
+				<input name="categories" type="text" value={values.categories} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.categories}</FormError>
 			</div>
 			<div className="form-group">
 				<label htmlFor="authorName">Author</label>
-				<input name="authorName" type="text" value={values.authorName} onChange={handleChange} />
-				<FormError>{errors.authorName}</FormError>
+				<input name="authorName" type="text" value={values.authorName} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.authorName}</FormError>
 			</div>
 			<div className="form-group">
 				<label htmlFor="publisher">Publisher</label>
-				<input name="publisher" type="text" value={values.publisher} onChange={handleChange} />
-				<FormError>{errors.publisher}</FormError>
+				<input name="publisher" type="text" value={values.publisher} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.publisher}</FormError>
 			</div>
 			<div className="form-group">
 				<label htmlFor="year">Year</label>
-				<input name="year" type="text" value={values.year} onChange={handleChange} />
-				<FormError>{errors.year}</FormError>
+				<input name="year" type="text" value={values.year} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.year}</FormError>
 			</div>
 			<div className="form-group">
 				<label htmlFor="pageNumbers">Page numbers</label>
-				<input name="pageNumbers" type="text" value={values.pageNumbers} onChange={handleChange} />
-				<FormError>{errors.pageNumbers}</FormError>
+				<input name="pageNumbers" type="text" value={values.pageNumbers} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.pageNumbers}</FormError>
 			</div>
 			<div className="form-group">
 				<label htmlFor="ISBN">ISBN-10 / ISBN-13</label>
-				<input name="ISBN" type="text" value={values.ISBN} onChange={handleChange} />
-				<FormError>{errors.ISBN}</FormError>
+				<input name="ISBN" type="text" value={values.ISBN} onChange={handleChange} onBlur={handleBlur} />
+				<FormError visible={touched}>{errors.ISBN}</FormError>
 			</div>
 			<div className="form-group">
-				<button type="submit" className="button button--primary" onChange={handleChange}>Save</button>
+				<button type="submit" disabled={!valid} className="button button--primary" onChange={handleChange} onBlur={handleBlur}>Save</button>
 			</div>
 		</form>
 	);
