353788d5a3434ff3f7486bd65c360d9764840f0b
Christos Ziogas <cziogas@steficon.gr>
2020-04-03 22:32:00 +0300

    Update root.js
    WIp selector for getting books by isbn

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,6 +1,18 @@
 import { combineReducers } from 'redux';
-import books from 'reducers/books';
+import books, * as fromBooks from 'reducers/books';
 import dialog from 'reducers/dialog';
+
+
+/**
+ * This function is used to colocate getBooksByIsbn selector with message reducer
+ * this helps consumer to not have to remember the state structure if changes
+ * @param {*} state
+ * @param {*} isbn
+ *
+ * @returns function
+ */
+
+export const getBooksByIsbn = (state, isbn) => fromBooks.getBooksByIsbn(state.books.list, isbn);
 
 export default combineReducers({
 	books,
