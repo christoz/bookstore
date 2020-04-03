d261bd969459c65cfc2011a3f137e910e022b341
Christos Ziogas <cziogas@steficon.gr>
2020-04-03 22:34:17 +0300

    Update index.js
    Memoized color value, added onRate binding to liftyp rating state

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import randomColor from 'randomcolor';
 
 import Rating from 'components/Rating';
-import ProductWrapper from './Product';
+import ProductWrapper from './ProductWrapper';
+import { rateBook } from 'actions/books';
 
 /**
  * Product list item
  * @param {Object} props
  */
 const Product = props => {
-	const color = randomColor({ hue: 'purple', luminosity: 'light' });
+	const dispatch = useDispatch();
+	const colorOptions = { hue: 'purple', luminosity: 'light' };
+	// We memoize color since we want to run once
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const color = useMemo(() => randomColor(colorOptions), []);
+
+	function rateProduct(rate) {
+		dispatch(rateBook(props.isbn, rate))
+	}
 
 	return (
 		<ProductWrapper>
@@ -21,7 +31,7 @@ const Product = props => {
 				</h4>
 				<div>{props.author}</div>
 				<div>
-					<Rating stars={5} />
+					<Rating stars={5} rating={props.rating} onRate={rateProduct} />
 				</div>
 			</ProductWrapper.Details>
 		</ProductWrapper>
@@ -32,7 +42,8 @@ Product.propTypes = {
 	img: PropTypes.any,
 	title: PropTypes.string,
 	id: PropTypes.number,
-	author: PropTypes.string
+	author: PropTypes.string,
+	rating: PropTypes.number
 };
 
 export default Product;
