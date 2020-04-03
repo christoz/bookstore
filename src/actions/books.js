3b97fc2532f3631f9f3aa2ce544e20c658c977ed
Christos Ziogas <cziogas@steficon.gr>
2020-04-03 22:31:28 +0300

    Action creators and state management for add book and rating

--- src/actions/books.js
+++ src/actions/books.js
@@ -1,7 +1,34 @@
-import { GET_BOOKS } from 'constants/ActionTypes';
+import { ADD_BOOK, RATE_BOOK } from 'constants/ActionTypes';
 
-export const getBooks = () => (dispatch) => {
+/**
+ * This action creator creates an action to add a book,
+ * We return a promise as it fits well to know when it's done to perform UX actions
+ *
+ * @param {Object} book
+ * @returns {Promise}, resolved promise
+ */
+export const addBook = book => dispatch => {
+	return Promise.resolve(
+		dispatch({
+			type: ADD_BOOK,
+			payload: book
+		})
+	);
+}
+
+/**
+ * This action creator creates and dispatches an action to rate a book,
+ *
+ * @param {Number} isbn
+ * @param {Intege} rating
+ * @returns {Object}
+ */
+export const rateBook = (isbn, rating) => dispatch => {
 	dispatch({
-		type: GET_BOOKS
+		type: RATE_BOOK,
+		payload: {
+			isbn,
+			rating
+		}
 	});
 }
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,5 +1,5 @@
 import createReducer from 'utils/createReducer';
-import { INIT_BOOKS } from 'constants/ActionTypes';
+import { INIT_BOOKS, ADD_BOOK, RATE_BOOK } from 'constants/ActionTypes';
 
 import { books } from 'data/books';
 
@@ -8,8 +8,40 @@ const initialState = {
 	loading: false
 };
 
+
+export const getBooksByIsbn = (state, isbn) => {
+	return state.filter(b => b.isbn === isbn);
+};
+
 export default createReducer(initialState, {
 	[INIT_BOOKS]: () => ({
 		...initialState
-	})
+	}),
+	[ADD_BOOK]: (state, action) => {
+		return {
+			...state,
+			list: [
+				...state.list,
+				action.payload
+			]
+		}
+	},
+	[RATE_BOOK]: (state, action) => {
+		const { isbn, rating } = action.payload;
+
+		return {
+			...state,
+			list: state.list
+				.map((book, index) => {
+					if (book.isbn === isbn) {
+						return {
+							...book,
+							rating
+						}
+					}
+
+					return book;
+				})
+		}
+	}
 });
