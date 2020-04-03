631b802403ef73adf4dbe66537052885a1393d1d
Christos Ziogas <cziogas@steficon.gr>
2020-04-03 18:53:35 +0300

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,6 @@ const theme = {
 	colors: {
 		baseLinkColor: '#9d77ee',
 		baseLinkColorHover: '#8b6ad1',
-		checkboxFontColor: '#a785f0',
-		lightBackgroundColor: '#f8f8f9',
-		primaryColor: '#232b36',
-		darkPrimaryColor: '#171c23',
 		secondaryLightColor: '#9aaecb',
 		baseBorderColor: '#d5d5d5',
 		baseBorderColorLight: '#ebebeb',
@@ -13,7 +9,6 @@ const theme = {
 		white: '#fff',
 		gray: '#ebebeb',
 		darkGray: '#8a8a8a',
-		inactiveLabelColor: '#7b7b7b',
 		errorColor: '#e74c3c',
 		successColor: '#10e547',
 		overlayColor: 'rgba(0, 0, 0, 0.24)'
@@ -23,11 +18,9 @@ const theme = {
 		primaryFallBack: '\'Arial\', \'Helvetica Neue\', \'Helvetica\', sans-serif default'
 	},
 	dimensions: {
-		baseFontSize: '16px',
-		baseGutter: '15px',
-		baseBorderRadius: '3px',
-		labelFontSize: '12px',
-		elementsGutter: '12px'
+		baseFontSize: '1rem',
+		baseGutter: '20px',
+		baseBorderRadius: '4px',
 	},
 	grid: {
 		gutterWidth: '2rem'
