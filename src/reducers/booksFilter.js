632a3c032b56af73df295b9a6cc3a7c23975fede
Christos Ziogas <cziogas@steficon.gr>
2020-04-04 16:43:01 +0300

    Map search and filtering actions action  with reducer

--- src/reducers/booksFilter.js
+++ src/reducers/booksFilter.js
@@ -1,17 +1,24 @@
 import createReducer from 'utils/createReducer';
 
-import { SHOW_ALL, SET_FILTER } from 'constants/ActionTypes';
+import { SHOW_ALL, SET_FILTER, SET_QUERY } from 'constants/ActionTypes';
 
 const initialState = {
 	filter: 'SHOW_ALL'
 };
 
+export const getSelectedFilter = state => state.filter;
+export const getBookQuery = state => state.query;
+
 export default createReducer(initialState, {
 	[SHOW_ALL]: (state) => ({
 		...state
 	}),
-	[SET_FILTER]: (state, filter) => ({
+	[SET_FILTER]: (state, action) => ({
 		...state,
-		filter
+		filter: action.filter
+	}),
+	[SET_QUERY]: (state, action) => ({
+		...state,
+		query: action.query
 	})
 });
