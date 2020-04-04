f86823d9b4337c18ee2fe5f9d4105baf0e473dad
Christos Ziogas <cziogas@steficon.gr>
2020-04-04 16:42:31 +0300

    Selector colocates

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,20 +1,37 @@
 import { combineReducers } from 'redux';
 import books, * as fromBooks from 'reducers/books';
+import booksFilter, * as fromBooksFilter from 'reducers/booksFilter'
 import dialog from 'reducers/dialog';
 
+/**
+ * This function is used to colocate getVisibleBooks selector with books reducer
+ * @param {Object} state
+ * @param {String} filter
+ *
+ * @returns {Function}
+ */
+export const getVisibleBooks = (state, filter) => fromBooks.getVisibleBooks(state.books.list, filter);
 
 /**
- * This function is used to colocate getBooksByIsbn selector with message reducer
- * this helps consumer to not have to remember the state structure if changes
- * @param {*} state
- * @param {*} isbn
+ * This function is used to colocate getSelectedFilter selector with bookFilters reducer
+ * @param {Object} state
+ * @parem {String} query
  *
- * @returns function
+ * @returns {Function}
  */
+export const getSelectedFilter = (state) => fromBooksFilter.getSelectedFilter(state.booksFilter);
 
-export const getBooksByIsbn = (state, isbn) => fromBooks.getBooksByIsbn(state.books.list, isbn);
+/**
+ * This function is used to colocate getBookQuery selector with bookFilters reducer
+ * @param {Object} state
+ * @parem {String} query
+ *
+ * @returns {Function}
+ */
+export const getBookQuery = (state) => fromBooksFilter.getBookQuery(state.booksFilter);
 
 export default combineReducers({
 	books,
-	dialog
+	dialog,
+	booksFilter
 });
